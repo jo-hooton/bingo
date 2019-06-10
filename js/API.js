d879ce66e531300660ea48cf105f1c4cde7a8f07
Joanne Hooton <jhooton@EM-02342-MBP.local>
2019-06-10 10:26:49 +0100

    slang object done

--- js/API.js
+++ js/API.js
@@ -1,5 +1,11 @@
 class API {
 
+  getPhrase = newNumber =>
+    const number = String(newNumber)
+    fetch('slang.json')
+      .then(res => res.json())
+      .then(json => json.phrases.number)
+
   getGif = number =>
     fetch(`http://api.giphy.com/v1/gifs/random?q=${number}&api_key=sytH0PbeMCXrff6ySBcJTexa4IP6n9ez&limit=1`)
       .then(res => res.json())
