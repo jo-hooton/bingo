f35b6a3248851caad491e3a8d898e9c9ec461140
Joanne Hooton <jhooton@EM-02342-MBP.local>
2019-06-17 14:30:56 +0100

    https in API

--- js/API.js
+++ js/API.js
@@ -1,7 +1,7 @@
 class API {
 
   getGif = phrase => {
-    return fetch(`http://api.giphy.com/v1/gifs/search?q=${phrase}&api_key=sytH0PbeMCXrff6ySBcJTexa4IP6n9ez&limit=1`)
+    return fetch(`https://api.giphy.com/v1/gifs/search?q=${phrase}&api_key=sytH0PbeMCXrff6ySBcJTexa4IP6n9ez&limit=1`)
       .then(res => res.json())
       .then(json => json.data[0].images.original.url)
   }
